1dcc23ebc5db2607192a302c5bb67c4c92167bd8
Rafael Coutinho <coutinhor@knowfully.com>
2023-05-30 20:01:26 -0300

    added initial classes from Domain

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveRequest : BaseEntity
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType? LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public DateTime DateRequested { get; set; }
+
+        public string? RequestComments { get; set; }
+
+        public bool? Approved { get; set; }
+
+        public bool? Cancelled { get; set; }
+
+        public string RequestingEmpoyeeId { get; set; } = string.Empty;
+    }
+}
