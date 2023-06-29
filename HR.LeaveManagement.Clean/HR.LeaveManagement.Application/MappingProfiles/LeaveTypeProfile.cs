6265f578fca18b2f498550636b43d2f6c5fd8740
Rafael Coutinho <coutinhor@knowfully.com>
2023-06-29 12:04:20 -0300

    added AutoMapper and MediatR

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+        }
+    }
+}
