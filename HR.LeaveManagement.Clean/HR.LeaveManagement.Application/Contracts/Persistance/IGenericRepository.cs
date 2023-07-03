da6da02e157ff1bf86087eacbe69bf9548d82218
Rafael Coutinho <coutinhor@knowfully.com>
2023-07-03 11:50:19 -0300

    Added a

--- HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Contracts/Persistance/IGenericRepository.cs
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Contracts/Persistance/IGenericRepository.cs
@@ -8,7 +8,7 @@ namespace HR.LeaveManagement.Application.Contracts.Persistance
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> GetAsync();
+        Task<List<T>> GetAsync();
         Task<T> GetByIdAsync(int id);
         Task<T> CreateAsync(T entity);
         Task<T> UpdateAsync(T entity);
